01a22c29b34e5ecc9e166ab2c19ef8d4f6d1d52c
Alec Dewulf <alec@cisudbury.ca>
2022-05-15 14:26:02 -0400

    initial rendering of 3d models

---
+++ src/components/Box.tsx
@@ -0,0 +1,10 @@
+const Box = () => {
+    return (
+        <mesh rotation={[90, 0, 20]}>
+            <boxBufferGeometry attach="geometry" args={[3, 3, 3]}/>
+            <meshLambertMaterial attach="material" color="blue"/>
+        </mesh>
+    )
+}
+
+export default Box;
