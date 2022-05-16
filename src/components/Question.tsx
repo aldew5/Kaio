e8745bae2f01f4f56924c209bcf0b4119bee2688
Alec Dewulf <alec@cisudbury.ca>
2022-05-16 18:16:45 -0400

    basic faq questions structure

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,20 +1,34 @@
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import { useState } from "react";
 
 interface QuestionProps {
     question: string;
     response: string;
 }
 
-const Question = ({question, response}: QuestionProps) => {
+const Question = ({ question, response }: QuestionProps) => {
+    const [open, setOpen] = useState<boolean>(false);
+
     return (
-        <div style={{width:"700px", marginBottom:"2rem"}}>
-            <div style={{float: "right", marginRight:"-10px"}}>
-                <ArrowDropDownIcon />
+        <div style={{ width: "700px", marginBottom: "2rem" }}>
+            <div style={{ float: "right", marginRight: "-10px" }}>
+                {(open) ?
+                    <ArrowDropUpIcon onClick={() => { setOpen(!open) }} /> :
+                    <ArrowDropDownIcon onClick={() => { setOpen(!open) }} />
+                }
             </div>
             <div>
                 {question}
-                <hr style={{width:"700px"}} />
             </div>
+            {(open) ?
+                <div style={{marginTop:"1rem"}}>
+                    {response}
+                </div>
+                :
+                <></>
+            }
+            <hr style={{ width: "700px" }} />
         </div>
     )
 }
