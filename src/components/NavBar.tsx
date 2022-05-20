2ee68c8303a3cc1389bb92f730b00714494d3470
Alec Dewulf <alec@cisudbury.ca>
2022-05-19 22:54:25 -0400

    completed base landing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,9 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { useNavigate } from "react-router-dom";
-import styles from "../styles/NavBar.module.css";
-import GoldLogo from "../assets/Gold.png";
+import GoldLogo from "../assets/Gold_nobg.png";
+import Logo from "../assets/Logo.png";
 
 const NavBar = () => {
     return (
@@ -14,13 +11,16 @@ const NavBar = () => {
             <Box component="div" sx={{ flexGrow: 1 }}>
                 <AppBar position="sticky" style={{ background: "transparent", boxShadow: "none" }}>
                     <Toolbar>
-                        <div style={{ backgroundColor:"black", borderRadius:"5px", paddingLeft:"20px", 
-                    paddingRight:"20px", paddingTop: "5px", paddingBottom:"5px"}}>
+                    <img src={Logo} alt="not found" style={{ width: "50px" }} />
+                        <div style={{
+                           paddingLeft: "20px",
+                            paddingRight: "10px", paddingTop: "5px", paddingBottom: "5px"
+                        }}>
                             <img src={GoldLogo} alt="not found" style={{ width: "90px" }} />
                         </div>
-                        <Button style={{borderRadius: 8,backgroundColor:"black"}} variant='contained' sx={{ marginLeft: "62rem" }}>CREW</Button>
-                        <Button style={{borderRadius: 8,backgroundColor:"black", marginLeft: "15px"}} variant='contained' > MAP</Button>
-                        <Button style={{borderRadius: 8,backgroundColor:"black", marginLeft: "15px"}} variant='contained' >FAQ</Button>
+                        <Button style={{ borderRadius: 8, backgroundColor: "black" }} variant='contained' sx={{ marginLeft: "62rem" }}>CREW</Button>
+                        <Button style={{ borderRadius: 8, backgroundColor: "black", marginLeft: "15px" }} variant='contained' > MAP</Button>
+                        <Button style={{ borderRadius: 8, backgroundColor: "black", marginLeft: "15px" }} variant='contained' >FAQ</Button>
                     </Toolbar>
                 </AppBar>
             </Box>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,21 +3,21 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 
 const SideBar = () => {
     return (
-        <div style={{position: "sticky", top: "70px", marginRight:"10px", float:'left'}}>
+        <div style={{position: "sticky", top: "70px", marginRight:"20px", float:'left', width:"25px", marginLeft: "10px"}}>
             <div style={{
                 width: "6px", height: "200px", backgroundColor: "black",
                 zIndex: "100", marginLeft: "10px", marginBottom: "10px"
             }}>
 
             </div>
-            <div>
+            <div style={{marginLeft:"1px"}}>
                 <TwitterIcon />
             </div>
-            <div style={{marginTop: "10px"}}>
+            <div style={{marginTop: "10px", marginLeft:"1px"}}>
                 <InstagramIcon/>
             </div>
             <div style={{
-                width: "6px", height: "200px", backgroundColor: "black",
+                width: "6px", height: "400px", backgroundColor: "black",
                 zIndex: "100", marginLeft: "10px", display: "flex", float: "left", marginTop: "10px"
             }}></div>
         </div>
