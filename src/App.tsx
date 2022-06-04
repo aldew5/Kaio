15346911a4cf046e9307f1b21cd352cd0579428c
Alec Dewulf <alec@cisudbury.ca>
2022-06-03 23:47:19 -0400

    lore page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Landing from "./pages/Landing";
 import Crew from "./pages/Crew";
 import FAQ from "./pages/FAQ";
 import Map from "./pages/Map";
+import Lore from "./pages/Lore";
 
 function App() {
   return (
       <div className='App'>
         <Landing />
+        <Lore />
         <Map />
         <Crew />
         <FAQ />
---
+++ src/pages/Lore.tsx
@@ -0,0 +1,46 @@
+const Lore = () => {
+    return (
+        <div style={{textAlign:"left", marginLeft: "5rem", marginRight: "5rem", 
+            backgroundColor: "#EEEEEE", fontFamily: 'Koulen cursive', fontSize:"20px"}}>
+            <h3>Kiao Lore</h3>
+            <p>
+                The days of the sea have proven to be lucrative for some,
+                and a series of disappointments for others. The ocean does not give way for anyone,
+                and ships have come and gone in the waters that do not sleep, for the churning of the
+                sea does not yield for anyone.
+            </p>
+            <p>
+                Years ago, towards the end of his life, a pirate captain once thought to be the world’s greatest,
+                began implementing his legacy. He buried treasure on an island far out in the ocean, one no one has ever
+                traveled to, treasure that would complete a seafarer’s life. The treasure, of course, could not be given away
+                nor revealed in an easy manner. That is why Kaio decided to create a map, one that would lead to the treasure, and ripped
+                it apart so one would have to piece it back together to find the location of his loot.
+            </p>
+            <p>
+                He spent the remaining years of his life traveling the world, scattering the pieces of the map around various ports and cities and landmarks,
+                with not even his crewmates knowing of the location. The treasure, of course, was already buried where it would remain for many years, and when
+                Kaio left this world, his senior deckhand was told of the first clue, one that would lead to the first piece of the map. The deckhand was told to tell
+                other crewmates of other ships, but pirates can be sneaky and he thought to keep the secret to himself. If it was not for another crewmate overhearing him
+                talking to himself about buried treasure, the deckhand might have succeeded.
+            </p>
+            <p>
+                As word spread, pirates from around the world began implementing their search for the pieces of the map. They scoured far and wide,
+                yet no one had been able to find the first piece, let alone complete the map. It was not until years later, when one pirate captain ventured into a
+                jungle temple on the southern coast of a remote island that he found the first piece, yet it turned out to be a fake. However, from here, the
+                journey for Kaio’s treasure truly began, and as more pirates began finding more clues to where pieces of the map were, the race to finish became more
+                competitive than ever. Battles on the sea on the location of these clues took place frequently, and those who were not truly geared towards finding the
+                treasure soon succumbed to the pressure and gave up. Notorious pirate captains seemed to shy away from even the reward that the treasure promised, for the
+                road ahead was not easy. This motivated other pirates even more however, and the hunt for the treasure ramped up even more. Yet, many others continued to try,
+                but pieces of the true map could not be found.
+
+            </p>
+            <p>
+                As the map remains unfinished, and the alluring loot of Kaio’s treasure is still available, only pirates bold enough to venture out into waters unknown will be rewarded with
+                what lies at the end of this adventure.
+            </p>
+
+        </div>
+    )
+}
+
+export default Lore;
