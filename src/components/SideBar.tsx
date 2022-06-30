c2e05da6418285b79482f23a1bb10b4d399228e6
Alec Dewulf <alec@cisudbury.ca>
2022-06-29 22:19:47 -0400

    socials

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,7 @@
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import TikTok from "../assets/tiktok.png";
+import Discord from "../assets/discord.png";
 import { IconButton } from '@mui/material';
 
 const SideBar = () => {
@@ -27,9 +29,22 @@ const SideBar = () => {
                         } />
                 </IconButton>
             </div>
-
+            <div style={{ marginTop: "10px", marginLeft: "-11px" }}>
+                <IconButton sx={{ color: "white" }}>
+                    <img src={Discord}
+                        onClick={() => { window.location.href = "https://discord.gg/ZcKsWg9PzJ"; }
+                        } />
+                </IconButton>
+            </div>
+            <div style={{ marginTop: "10px", marginLeft: "-10px" }}>
+                <IconButton sx={{ color: "white" }}>
+                    <img src={TikTok} width="25px"
+                        onClick={() => { window.location.href = "https://www.tiktok.com/@nft_kaio"; }
+                        } />
+                </IconButton>
+            </div>
             <div style={{
-                width: "2px", height: "300px", backgroundColor: "white",
+                width: "2px", height: "240px", backgroundColor: "white",
                 zIndex: "100", marginLeft: "10px", display: "flex", float: "left", marginTop: "10px"
             }}></div>
         </div>
--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,6 +1,8 @@
 import styles from "../styles/Footer.module.css";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import TikTok from "../assets/tiktok-brown.png";
+import Discord from "../assets/discord-brown.png";
 import { IconButton } from "@mui/material";
 
 const Footer = () => {
@@ -18,17 +20,31 @@ const Footer = () => {
                             Terms of Use
                         </div>
                     </div>
-                    <div style={{ display: "table-cell", paddingLeft: "400px", width: "100px" }}>
+                    <div style={{ display: "table-cell", paddingLeft: "400px", paddingRight: "20px" }}>
                         <IconButton sx={{color: "#837456"}}>
                             <InstagramIcon
                                 onClick={() => { window.location.href = "https://www.instagram.com/nft_kaio/"; }
                                 } />
                         </IconButton>
                     </div>
-                    <div style={{ display: "table-cell" }}>
+                    <div style={{ display: "table-cell", paddingRight: "20px" }}>
                         <IconButton>
                             <TwitterIcon
                                 onClick={() => { window.location.href = "https://twitter.com/Nft_Kaio"; }
+                                } />
+                        </IconButton>
+                    </div>
+                    <div style={{ display: "table-cell", paddingRight: "20px" }}>
+                        <IconButton>
+                            <img src={TikTok} height="20px"
+                                onClick={() => { window.location.href = "https://twitter.com/Nft_Kaio"; }
+                                } />
+                        </IconButton>
+                    </div>
+                    <div style={{ display: "table-cell" }}>
+                        <IconButton>
+                            <img src={Discord} height="20px"
+                                onClick={() => { window.location.href = "https://www.tiktok.com/@nft_kaio"; }
                                 } />
                         </IconButton>
                     </div>
